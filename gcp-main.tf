d091f026fb46ff021349f66d40b812c6700eb569
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2022-05-28 14:31:35 -0300

    Fix Region

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,12 +1,12 @@
 resource "google_app_engine_application" "app" {
   project     = "labdevops-t05-2"
-  location_id = "southamerica-west1"
+  location_id = "southamerica-east1"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "southamerica-west1"
+  location = "southamerica-east1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
